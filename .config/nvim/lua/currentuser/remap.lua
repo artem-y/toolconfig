3b2c05d12e7491dc46124a0d7c1315e8ae38cd2d
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2025-01-24 01:32:39 +0200

    Add telescope remap descriptions

--- .config/nvim/lua/currentuser/remap.lua
+++ .config/nvim/lua/currentuser/remap.lua
@@ -59,8 +59,8 @@ vim.keymap.set("n", "<leader>html", "i<!Doctype HTML>\
 
 -- Add Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
-vim.keymap.set("n", "<leader>f?", builtin.builtin, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "buffers" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find_files" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "help_tags" })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "keymaps" })
+vim.keymap.set("n", "<leader>f?", builtin.builtin, { desc = "builtin" })
