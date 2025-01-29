fdc4eda6a8eb3254e493f00e6e7fcbe146c18285
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2025-01-29 23:10:22 +0200

    Install Go LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,4 +25,27 @@ vim.api.nvim_create_autocms("FileType", {
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
 })
+
+-- Launch Go LSP
+require("lspconfig").gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+  on_attach = function(client, bfr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("GoFormat", { clear = true }),
+        buffer = bfr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end
+      })
+    end
+  end,
 })
