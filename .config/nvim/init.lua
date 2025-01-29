a8d9e78be60b4f27bf7fa45d6b5053536b6f4bac
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2025-01-29 23:14:31 +0200

    Fix lua filetype autocmd syntax

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,12 +18,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocms("FileType", {
+vim.api.nvim_create_autocmd("FileType", {
   pattern = { "lua" },
   callback = function()
     vim.opt_local.shiftwidth = 2
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
+  end,
 })
 
 -- Launch Go LSP
