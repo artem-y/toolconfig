b7fc8b1e929b248abba43940889ddceebfc762c9
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2025-02-02 18:48:22 +0200

    Remap to open diagnostic message in floating panel

--- .config/nvim/lua/currentuser/remap.lua
+++ .config/nvim/lua/currentuser/remap.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>r", vim.cmd.Rex)
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<leader>jd", ":lua vim.lsp.buf.definition()\n")
+vim.keymap.set("n", "<leader>fl", ":lua vim.diagnostic.open_float()\n")
 
 -- Enclose word in normal mode 
 vim.keymap.set("n", "<leader>(", "viwdi()<C-[>hp")
